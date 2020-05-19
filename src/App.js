000fa89176c8efa4e94cb41a4b22cc881fe15fc2
Richard Dang <richardspacedang@gmail.com>
2020-05-19 00:02:45 -0400

    Finish 6.13 - 6.14

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 import Filter from "./components/Filter";
+import { useDispatch } from "react-redux";
+import anecdotesService from "./services/anecdotes";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchAnecdotes = async () => {
+      const anecdotes = await anecdotesService.getAll();
+      dispatch(initializeAnecdotes(anecdotes));
+    };
+    fetchAnecdotes();
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
